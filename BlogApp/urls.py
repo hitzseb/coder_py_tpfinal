c20463616ff7f4a69c234d5e3ba6dc49bcc68736
unknown <hitzseb@gmail.com>
2022-11-27 00:11:35 -0300

    templates,admin

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from BlogApp import views
 
 urlpatterns = [
-    path('home/', views.home),
-    path('post/', views.post),
-    path('category/', views.category),
-    path('author/', views.author),
+    path('home/', views.home, name = 'Home'),
+    path('posts/', views.posts, name = 'Posts'),
+    path('category/', views.category, name = 'Category'),
+    path('author/', views.author, name = 'Author'),
+    path('postsApi/', views.apiposts),
 ]
--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from BlogApp.models import Post
+from django.core import serializers
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'BlogApp/index.html')
 
-def post(request):
-    return HttpResponse('post')
+def posts(request):
+    return render(request, 'BlogApp/posts.html')
 
 def category(request):
     return HttpResponse('category')
 
 def author(request):
     return HttpResponse('author')
+
+def apiposts(request):
+    all_posts = Post.objects.all()
+    return HttpResponse(serializers.serialize('json', all_posts))
